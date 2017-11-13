f5f1689f140b1e9b8b98335ab1351952e9dc22f0
jaric-de <yaroslav.decik@gmail.com>
2017-11-13 18:10:38 +0200

    Laravel ide helper init

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          * Package Service Providers...
          */
         Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Application Service Providers...
