1b3d29b109f0ccb549da87c291a401038c08b84d
jaric-de <yaroslav.decik@gmail.com>
2017-11-13 19:13:01 +0200

    Game instance init

---
+++ database/migrations/2017_11_13_162330_create_games_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('title');
+            $table->string('platform');
+            $table->string('genre');
+            $table->double('price');
+            $table->text('description');
+            $table->timestamp('release_date');
+            $table->double('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
